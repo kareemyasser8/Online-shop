3eb71f3581c558a1010a52e6b9ed699729de8d0e
kareemyasser8 <kareem.yasserr@gmail.com>
2023-05-07 12:18:18 +0200

    updating environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   firebase: {
-    projectId: 'fir-demo-8c455',
-    appId: '1:711512823969:web:7da60e3a8056d65d3af8e0',
-    databaseURL: 'https://fir-demo-8c455-default-rtdb.firebaseio.com',
-    storageBucket: 'fir-demo-8c455.appspot.com',
-    apiKey: 'AIzaSyDZi_vmn-FqkmQ91wfFi8vGjQBTrm1rTzk',
-    authDomain: 'fir-demo-8c455.firebaseapp.com',
-    messagingSenderId: '711512823969',
-    measurementId: 'G-SZ581LPBGS',
+    projectId: 'o-shop-b4bb8',
+    appId: '1:688508207066:web:f272eb106b63695c230ae3',
+    databaseURL: 'https://o-shop-b4bb8-default-rtdb.firebaseio.com',
+    storageBucket: 'o-shop-b4bb8.appspot.com',
+    apiKey: 'AIzaSyBeEBEXUNTzQTZv31ASG51vxBflNuYqlu8',
+    authDomain: 'o-shop-b4bb8.firebaseapp.com',
+    messagingSenderId: '688508207066',
+    measurementId: 'G-MRKFBCKX44',
   },
   production: true
 };
+
