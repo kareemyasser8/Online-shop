aef398595e976a8773a65ec19c0173ec1e23f50f
kareemyasser8 <kareem.yasserr@gmail.com>
2023-05-16 11:02:20 +0200

    fixing the + and - btns in product-card-footer

--- src/app/shopping/components/products/products.component.ts
+++ src/app/shopping/components/products/products.component.ts
@@ -1,7 +1,7 @@
 import { ShoppingCartService } from '../../../shared/services/shopping-cart.service';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../../shared/services/product.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { Subject, switchMap, Subscription, Observable } from 'rxjs';
 import { Product } from '../../../shared/models/product';
 import { ShoppingCart } from '../../../shared/models/shopping-cart';
@@ -20,18 +20,23 @@ export class ProductsComponent implements OnInit {
   dtTrigger: Subject<any> = new Subject<any>();
   productsNormal;
   cart$: Observable<ShoppingCart>;
+  windowWidth: number;
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
     private shoppingCartService: ShoppingCartService
-  ) {
+  ) {}
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.windowWidth = event.target.innerWidth;
   }
 
   async ngOnInit() {
     this.cart$ = await this.shoppingCartService.getCart()
     this.populateProducts();
+    this.windowWidth = window.innerWidth;
   }
 
   private populateProducts() {
